bac98c06c2ad3580b6ea27fcf6dc3bfe7a013566
Bodhisattwa <mbodhisattwa@gmail.com>
2020-02-15 23:31:18 -0800

    checkpoint naming

--- models/transfer_baseline/train.py
+++ models/transfer_baseline/train.py
@@ -256,7 +256,7 @@ def train():
 
     # On the main process: close tensorboard logger and rename the last checkpoint (for easy re-loading with OpenAIGPTModel.from_pretrained method)
     if args.local_rank in [-1, 0] and args.n_epochs > 0:
-        os.rename(checkpoint_handler._saved[-1][1][-1], os.path.join(log_dir, WEIGHTS_NAME))  # TODO: PR in ignite to have better access to saved file paths (cleaner)
+        os.rename(os.path.join(log_dir, checkpoint_handler._saved[-1][1]), os.path.join(log_dir, WEIGHTS_NAME))  # TODO: PR in ignite to have better access to saved file paths (cleaner)
         tb_logger.close()
 
 if __name__ == "__main__":
