0cfeff4bffc35181991e66dca34644dc704a8c8a
Harsh Jhamtani <harshjhamtani@gmail.com>
2020-04-01 13:52:36 -0700

    pretraining code bow

--- models/pretraining/model.py
+++ models/pretraining/model.py
@@ -3,6 +3,7 @@ from transformers import RobertaForSequenceClassification
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from models.reinforce_model.prior_posterior_models import PriorBoWModel, PriorRobertaModel
 
 '''
 Posterior Pretraining:
@@ -29,50 +30,19 @@ class PriorPretrainingModel(nn.Module):
         super().__init__()
 
         self.args = args
-        self.uniform_prior = args.uniform_prior
-        if not self.uniform_prior:
-            self.roberta_model = RobertaForSequenceClassification.from_pretrained('roberta-base', output_hidden_states=True)
-        # self.gpt2_model = generator_class.from_pretrained(args.model_checkpoint)
-        # self.criterion_lm = torch.nn.CrossEntropyLoss(ignore_index=-100, reduction='none')
+        if args.prior_model == 'bow':
+            self.prior_model = PriorBoWModel(args)
+        elif args.prior_model == 'roberta':
+            self.prior_model = PriorRobertaModel(args)
+        else:
+            raise Exception('Invalid prior model')
         self.criterion_mc = torch.nn.MSELoss() #reduction='none')
 
     def get_score_z_given_H(self, persona, history):
-        '''
-        persona: B x P x T
-        H: B x T
-
-        We take the pooled output from Roberta; which uses same tokenization as GPT2
-        TODO: Add <s> token at the beginning which will act as [CLS] token in BERT
-        '''
-        history_encodings = self.roberta_model(history)[1][-1][:, 0, :] # B x 764
-        history_encodings = history_encodings.unsqueeze(1).repeat(1, persona.shape[1], 1) # B x P x 764
-        persona_encodings = []
-        for i in range(persona.shape[1]):
-            persona_enc = self.roberta_model(persona[:, i, ...])[1][-1][:, 0, :] # B x 764
-            persona_encodings.append(persona_enc)
-        persona_encodings = torch.stack(persona_encodings, axis=1)
-        norms = -1.0 * torch.norm(history_encodings-persona_encodings, 2, dim=-1)
-        prob_z_given_H = F.softmax(norms, dim=-1)
-        return prob_z_given_H #  B x P
+        return self.prior_model.get_prob_z_given_H(persona=persona, history=history)
 
     def get_score_z_given_goldcandidate(self, persona, response):
-        '''
-        persona: B x P x T
-        H: B x T
-
-        We take the pooled output from Roberta; which uses same tokenization as GPT2
-        TODO: Add <s> token at the beginning which will act as [CLS] token in BERT
-        '''
-        response_encodings = self.roberta_model(response)[1][-1][:, 0, :] # B x 764
-        response_encodings = response_encodings.unsqueeze(1).repeat(1, persona.shape[1], 1) # B x P x 764
-        persona_encodings = []
-        for i in range(persona.shape[1]):
-            persona_enc = self.roberta_model(persona[:, i, ...])[1][-1][:, 0, :] # B x 764
-            persona_encodings.append(persona_enc)
-        persona_encodings = torch.stack(persona_encodings, axis=1)
-        norms = -1.0 * torch.norm(response_encodings-persona_encodings, 2, dim=-1)
-        prob_z_given_H = F.softmax(norms, dim=-1)
-        return prob_z_given_H #  B x P
+        return self.prior_model.get_prob_z_given_H(persona=persona, history=response)
 
 
     def forward(
@@ -92,20 +62,8 @@ class PriorPretrainingModel(nn.Module):
         mc_labels: B
         token_type_ids: B x P x C x T 
         '''
-
-        # print("mc_token_ids : ", mc_token_ids.size())
-        # print("persona : ", persona.size())
-        # print("history : ", history.size())
-        # print("mc_token_ids = ", mc_token_ids)
-        # print("mc_labels : ", mc_labels.size())
-        # print("mc_labels = ", mc_labels)
         mc_token_ids_gt = mc_token_ids[:,:,0]
-        # print("mc_token_ids_gt: ", mc_token_ids_gt.size())
         z_given_h = self.get_score_z_given_H(persona, history) # B x P - unnormalized scores
-        # print("****** z_given_h = ", z_given_h)
         desired_z = self.get_score_z_given_H(persona, mc_token_ids_gt) # B x P - unnormalized scores
-        # print("****** desired_z = ", desired_z)
         loss_prior_pretraining = self.criterion_mc(z_given_h, desired_z)
-        # print("****** loss_prior_pretraining = ", loss_prior_pretraining)
-        # mc_token_ids = mc_token_ids[:, i, ...].contiguous()
         return loss_prior_pretraining
