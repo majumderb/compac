715b7b27ca462a7e72296d0b930bd23270f067a8
Bodhisattwa <mbodhisattwa@gmail.com>
2020-04-09 15:01:47 -0700

    logging

--- models/reinforce_model/train.py
+++ models/reinforce_model/train.py
@@ -228,8 +228,8 @@ def train():
     RunningAverage(output_transform=lambda x: x[0]).attach(trainer, "loss")
     RunningAverage(output_transform=lambda x: x[1]).attach(trainer, "lm_loss")
     RunningAverage(output_transform=lambda x: x[2]).attach(trainer, "mc_loss")
-    RunningAverage(output_transform=lambda x: x[4]).attach(trainer, "prior_loss")
-    RunningAverage(output_transform=lambda x: x[5]).attach(trainer, "cond_lm_loss")
+    RunningAverage(output_transform=lambda x: x[3]).attach(trainer, "prior_loss")
+    RunningAverage(output_transform=lambda x: x[4]).attach(trainer, "cond_lm_loss")
 
     metrics = {
         "nll": Loss(torch.nn.CrossEntropyLoss(ignore_index=-100), output_transform=lambda x: (x[0][0], x[1][0])),
