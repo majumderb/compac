cdec626b554830224233056076bf0269d33e9197
Bodhisattwa <mbodhisattwa@gmail.com>
2020-04-09 15:11:48 -0700

    num labels

--- models/reinforce_model/model.py
+++ models/reinforce_model/model.py
@@ -96,14 +96,14 @@ class LatentMarginalizedModel(nn.Module):
                 if self.training_type == TRAINING_TYPE_MARGINALIZE:
                     log_prob_x_z_given_h += torch.log(z_given_h[:, i])  # B
 
-                log_probs_lm.append(log_prob_x_z_given_h)
+                log_probs_lm.append(log_prob_x_z_given_h / num_labels)
 
                 # MC
                 ll_mc = -1.0 * self.criterion_mc(mc_logits.view(-1, mc_logits.size(-1)), mc_labels_persona.view(-1))
                 ll_mc = ll_mc.view(mc_labels.size(0), -1).sum(-1)
 
                 log_prob_x_given_z_h_mc = ll_mc + torch.log(z_given_h[:, i])  # B
-                log_probs_mc.append(log_prob_x_given_z_h_mc / num_labels)
+                log_probs_mc.append(log_prob_x_given_z_h_mc)
 
             if self.training_type == TRAINING_TYPE_MARGINALIZE:
                 # LM
