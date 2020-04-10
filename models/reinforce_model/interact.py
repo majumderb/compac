70e158b05203de0a8563233d29b2b7f8d95fabb9
Bodhisattwa <mbodhisattwa@gmail.com>
2020-04-09 22:30:17 -0700

    selected persona

--- models/reinforce_model/interact.py
+++ models/reinforce_model/interact.py
@@ -76,6 +76,7 @@ def sample_sequence(personality, history, tokenizer, model, args, current_output
         prior_z = model.prior_model.get_prob_z_given_H(padded_persona_tensor, history_flat_tensor) # B x P
         # z = torch.argmax(prior_z, dim=1).item()
         z, _ = model.prior_model.sample(prior_z)
+        z = z.item()
 
     selected_personality = [personality[z]]
     print('Selected Persona {}'.format(z))
